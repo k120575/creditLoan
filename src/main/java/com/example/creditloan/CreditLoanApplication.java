eb9fd8f2d5fa6365547d9ec66ed267dcbcbf468b
k120575 <k120575@gmail.com>
2023-01-29 22:06:50 +0800

    fix wrong word

--- src/main/java/com/example/creditloan/CreditLoanApplication.java
+++ src/main/java/com/example/creditloan/CreditLoanApplication.java
@@ -25,7 +25,7 @@ public class CreditLoanApplication {
         // 每月平均潘還率 = { [ (1 + 月利率 ) ^ 期數 ] x 月利率 } / { [ ( 1 + 月利率 ) ^ 期數 ] - 1 }
         BigDecimal avgPaymentRate = ((monthlyInterestRate.add(BigDecimal.ONE)).pow(period)).multiply(monthlyInterestRate)
                 .divide(((monthlyInterestRate.add(BigDecimal.ONE)).pow(period)).subtract(BigDecimal.ONE), 5, BigDecimal.ROUND_HALF_UP);
-        System.out.println("每月平均潘還率 = " + avgPaymentRate);
+        System.out.println("每月平均攤還率 = " + avgPaymentRate);
         // 每月還款金額 = 貸款總金額 x 每月平均攤還率
         Integer monthlyRepayment = avgPaymentRate.multiply(new BigDecimal(loan)).intValue();
         System.out.println("每月還款金額 = " + monthlyRepayment);
